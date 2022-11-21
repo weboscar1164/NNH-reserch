0221ca268afa283d01bcb0e78985ad28844b9a93
webshoposcar1164 <cheaptrip1164@gmail.com>
2022-11-21 22:27:22 +0900

    first

---
+++ NNH-reserch/resources/js/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <main>
+            <div class="container">
+                <RouterView />
+            </div>
+        </main>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+const addCount = () => {
+    count.value++;
+};
+</script>
---
+++ NNH-reserch/resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>{{ config('app.name') }}</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merriweather|Roboto:400" />
+</head>
+
+<body>
+    <div id="app">
+
+    </div>
+</body>
+
+</html>
