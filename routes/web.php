ba695f9f4ba7c98b0c330ce9488d7309d3b1b6d7
webshoposcar1164 <cheaptrip1164@gmail.com>
2022-11-22 06:25:45 +0900

    reprace

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/{any?}', fn () =>
-view('index'))->where('any', '.+');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', function () {
+    return view('welcome');
+});
